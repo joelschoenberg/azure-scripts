fc76ed3b5946fffcd2c07f1b424f916ac2917481
joelschoenberg <joel.schoenberg@hotmail.com>
2017-10-10 23:15:34 -0700

    Added script for viewing custom schema

---
+++ RetrieveSchema.py
@@ -0,0 +1,11 @@
+# This script retrieves the Schema of a custom Analytics Event
+
+import requests
+
+url = 'http://34.210.157.84:9080/events/schema/azureFunctions'
+headers = {'X-Events-API-AccountName': 'customer1_b7686ddf-6680-43f3-bbeb-303c027344e2', 'X-Events-API-Key': '50c4c457-d91a-44b6-a6e1-437d1f35e50a', 'Accept': 'application/vnd.appd.events+json;v=1'}
+appd = requests.get(url, headers=headers)
+print "Accessing AppDynamics..."
+print "Return code:"
+print appd.status_code
+print appd.content
